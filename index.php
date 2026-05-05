b5b443294cb37e7fbaff3dab4370b6ddc4270632
Danang <danang@MacBook-Air-Danang.local>
2026-05-05 09:03:29 +0700

    f2

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hosting Danang</title>
+</head>
+<body>
+    <marquee behavior="scroll" direction="left">Selamat Datang di Hosting Danang</marquee>
+</body>
+</html>
