778f14cb75ad87cef0311b0f254436fd36216b79
Danang <danang@MacBook-Air-Danang.local>
2026-05-05 09:18:24 +0700

    sss

--- api.php
+++ api.php
@@ -1,5 +1,10 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json");
+
 include 'koneksi.php';
 
 $data = array();
