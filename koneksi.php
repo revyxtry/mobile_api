44980a0d86b0b3866085dc501092e28cbcb3223a
Danang <danang@MacBook-Air-Danang.local>
2026-05-05 09:13:00 +0700

    f22

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,11 @@
 <?php
-// Gunakan variabel environment Railway (otomatis terisi di server)
-$host = $_ENV['MYSQLHOST'] ?? "mysql-gwdn.railway.internal";
-$user = $_ENV['MYSQLUSER'] ?? "root";
-$pass = $_ENV['MYSQLPASSWORD'] ?? "xTelNxEetHsGNngapadiYHPlhxZkmjyH";
-$db   = $_ENV['MYSQLDATABASE'] ?? "railway";
-$port = $_ENV['MYSQLPORT'] ?? 3306; // Port internal biasanya 3306
+$host = "trolley.proxy.rlwy.net"; // Hanya domain, tanpa mysql://
+$user = "root";
+$pass = "xTelNxEetHsGNngapadiYHPlhxZkmjyH";
+$db   = "railway";
+$port = 39372; 
 
+// Menggunakan @ untuk menyembunyikan warning bawaan dan menggantinya dengan pesan custom
 $koneksi = @mysqli_connect($host, $user, $pass, $db, $port);
 
-if (!$koneksi) {
-    header('Content-Type: application/json');
-    die(json_encode(["error" => "Koneksi internal gagal"]));
-}
 ?>
